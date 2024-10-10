a28ca2483ff604cd768118f6f9f4a7013a593b43
jeerupraveen <21B91A5468@srkrec.ac.in>
2024-10-10 22:29:26 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -8,12 +8,27 @@ dotenv.config();
 let db;
 
 async function conToDb(cb) {
-    const url = process.env.MONGO_URL; // Ensure your .env file has this variable
-    const client = new MongoClient(url);
-    await client.connect();
-    db = client.db('ATTENDENCE_APP');
-    db.collection("userdata").createIndex({ email: 1 }, { unique: true })
-    cb();
+    try {
+        const url = process.env.MONGO_URL; // Ensure your .env file has this variable
+        const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+        
+        // Connect to the MongoDB client
+        await client.connect();
+        
+        // Select the database
+        db = client.db('ATTENDENCE_APP');
+
+        // Create an index on the email field in the userdata collection
+        await db.collection('userdata').createIndex({ email: 1 }, { unique: true });
+
+        console.log('Successfully connected to the database');
+
+        // Execute the callback function
+        cb();
+    } catch (err) {
+        console.error('Failed to connect to the database', err);
+        process.exit(1); // Exit the process with failure
+    }
 }
 
 // Export the connection function and the db variable
